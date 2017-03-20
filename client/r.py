a55c52541a501164216389197afaa245e56d80b6
harshit_mahajan <harshitmahajan0904@gmail.com>
2017-03-21 00:41:16 +0530

    Exchanged the role of client and server

--- client/r.py
+++ client/r.py
@@ -12,66 +12,32 @@ port = 60000
 sock.connect((host, port))
 
 def Main():
-    while True:
-        print('receiving data...')
-        data = sock.recv(1024)
-        if data=="Over":
-            break
-        print('msg = %s' %(data))
-        args = data.split()
-        if not args:
-            ls = "Please enter the command"
-            sock.send(ls)
-        elif args[0]== "index":
-            if len(args) == 1:
-                ls = os.popen('ls').read()
-            elif args[1] == "longlist" :
-                ls = os.popen('ls -l').read()
-            elif args[1] == "shortlist" :
-                ls = os.popen('ls').read()
-                fs = ls.split()
-                # fl =[]
-                for it in range(len(fs)):
-                    temp = get_mtime(fs[it])
-                    fs[it] = fs[it] + " " +str(temp)
-                ls = "\n".join(fs)
-            else:
-                ls = "please check the syntax"
-            sock.send(ls)
-        elif args[0] == "hash":
-            if len(args) < 2:
-                ck = "please provide arguments"
-            elif args[1] == "verify":
-                if not args[2]:
-                    ck = "Enter File name"
-                else:
-                    comm = "cksum " + args[2]
-                    ck = os.popen(comm).read()
-                # print ck
-            else:
-                ck = "please check the syntax"
-            sock.send(ck)
-        elif args[0] == "download" and len(args)==3:
-            if args[1] == "TCP"
-                with open(args[2]) as fileobject:
-                    for line in fileobject:
-                        sock.send(line)
-                time.sleep(1)
-                sock.send("zqqxq")
-            elif args[1] == "UDP"
-                with open(args[2]) as fileobject:
-                    for line in fileobject:
-                        sock.send(line)
-                time.sleep(1)
-                sock.send("zqqxq")
-            else:
-                ls = "Enter TCP or UDP as second argument"
-                sock.send(ls)
+    # print 'Server listening....'
+    # client, addr = sock.accept() # addr = (ip,port) , waiting for connection
+    l = raw_input("prompt> ")
+    while (l != "quit"):
+        sock.send(l) # send client message l
+        args = l.split()
+        if args[0] == "download":
+            data = "ktb"
+            fh = open(args[2], 'a+')
+            while True:
+                data = sock.recv(1024)
+                if data == "zqqxq":
+                    break
+                fh.write(data)
+            fh.close()
+            # print "checking MD5sum"
+            # print "Download complete!"
+            print "File "+args[2]
         else:
-            ls = "Please check command or Format"
-            sock.send(ls)
+            data = sock.recv(1024)
+            print data
+        l = raw_input("prompt> ")
+
+    print('Connection Closed, Bye!')
+    sock.send('Over')
     sock.close()
-    print('connection closed, Bye!')
 
 if __name__ == "__main__":
     Main()
--- server/s.py
+++ server/s.py
@@ -1,4 +1,10 @@
 import socket,os,time,sys,subprocess
+import datetime
+import hashlib
+
+def get_mtime(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
 
 sock = socket.socket()
 host = "127.0.0.1"
@@ -9,33 +15,68 @@ sock.bind((host, port))
 sock.listen(5) # 5 client concurrently connected
 
 def Main():
-    print 'Server listening....'
+    print('server starts...')
     client, addr = sock.accept() # addr = (ip,port) , waiting for connection
-    l = raw_input("prompt> ")
-    while (l != "quit"):
-        client.send(l) # send client message l
-        args = l.split()
-        if args[0] == "download":
-            data = "ktb"
-            fh = open(args[2], 'a+')
-            while True:
-                data = client.recv(1024)
-                if data == "zqqxq":
-                    break
-                fh.write(data)
-            fh.close()
-            print "checking MD5sum"
-            
-            print "Download complete!"
-            print "File "+args[2]
+    while True:
+        data = client.recv(1024)
+        print('request = %s' %(data))
+        if data=="Over":
+            break
+        args = data.split()
+        if not args:
+            ls = "Please enter the command"
+            client.send(ls)
+        elif args[0]== "index":
+            if len(args) == 1:
+                ls = os.popen('ls').read()
+            elif args[1] == "longlist" :
+                    ls = os.popen('ls -l').read()
+            elif args[1] == "shortlist" :
+                ls = os.popen('ls').read()
+                fs = ls.split()
+                # fl =[]
+                for it in range(len(fs)):
+                    temp = get_mtime(fs[it])
+                    fs[it] = fs[it] + " " +str(temp)
+                ls = "\n".join(fs)
+            else:
+                ls = "please check the syntax"
+            client.send(ls)
+        elif args[0] == "hash":
+            if len(args) < 2:
+                ck = "please provide arguments"
+            elif args[1] == "verify":
+                if not args[2]:
+                    ck = "Enter File name"
+                else:
+                    comm = "cksum " + args[2]
+                    ck = os.popen(comm).read()
+                # print ck
+            else:
+                ck = "please check the syntax"
+            client.send(ck)
+        elif args[0] == "download" and len(args)==3:
+            if args[1] == "TCP":
+                with open(args[2]) as fileobject:
+                    for line in fileobject:
+                        client.send(line)
+                time.sleep(1)
+                client.send("zqqxq")
+            elif args[1] == "UDP":
+                with open(args[2]) as fileobject:
+                    for line in fileobject:
+                        client.send(line)
+                time.sleep(1)
+                client.send("zqqxq")
+            else:
+                ls = "Enter TCP or UDP as second argument"
+                client.send(ls)
         else:
-            data = client.recv(1024)
-            print data
-        l = raw_input("prompt> ")
-
-    print('Connection Closed, Bye!')
-    client.send('Over')
+            ls = "Please check command or Format"
+            client.send(ls)
     client.close()
+    print('connection closed, Bye!')
+
 
 if __name__ == "__main__":
     Main()
